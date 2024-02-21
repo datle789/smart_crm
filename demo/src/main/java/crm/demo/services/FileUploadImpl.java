80dbdd4393a13fd0fbeaae158cf5869bb9c63f57
Lê Đạt Anh <datletb789@gmail.com>
2024-02-21 16:05:22 +0700

    add image

--- demo/src/main/java/crm/demo/services/FileUploadImpl.java
+++ demo/src/main/java/crm/demo/services/FileUploadImpl.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @RequiredArgsConstructor
@@ -17,15 +18,23 @@ public class FileUploadImpl implements FileUpload {
 
     private final Cloudinary cloudinary;
 
+    @Value("${max.file.size}") // Cấu hình dung lượng file tối đa từ application.properties
+    private long maxFileSize;
+
     @Override
-    public List<String> uploadFile(List<MultipartFile> multipartFile) throws IOException {
+    public List<String> uploadFile(List<MultipartFile> multipartFiles) throws IOException {
         List<String> successMessages = new ArrayList<>();
-        for (MultipartFile file : multipartFile) {
-            successMessages.add("File uploaded successfully: " + cloudinary.uploader()
-                    .upload(file.getBytes(),
-                            Map.of("public_id", UUID.randomUUID().toString()))
-                    .get("url")
-                    .toString());
+        for (MultipartFile file : multipartFiles) {
+            if (file.getSize() > maxFileSize) {
+                // File size exceeds limit, handle accordingly
+                successMessages.add("File size exceeds the limit: " + file.getOriginalFilename());
+            } else {
+                successMessages.add("File uploaded successfully: " + cloudinary.uploader()
+                        .upload(file.getBytes(),
+                                Map.of("public_id", UUID.randomUUID().toString()))
+                        .get("url")
+                        .toString());
+            }
         }
         return successMessages;
     }
