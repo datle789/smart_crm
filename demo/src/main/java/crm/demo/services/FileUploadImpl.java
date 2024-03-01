f354a576268b9693605388240b65e8b765de191b
Lê Đạt Anh <datletb789@gmail.com>
2024-03-01 17:11:58 +0700

    validate image

--- demo/src/main/java/crm/demo/services/FileUploadImpl.java
+++ demo/src/main/java/crm/demo/services/FileUploadImpl.java
@@ -3,6 +3,7 @@ package crm.demo.services;
 import com.cloudinary.Cloudinary;
 
 import crm.demo.controllers.CrmController;
+import crm.demo.utils.ErrorUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,24 +26,15 @@ public class FileUploadImpl implements FileUpload {
 
     private static final Logger logger = LoggerFactory.getLogger(CrmController.class);
 
-    @Value("${max.file.size}") // Cấu hình dung lượng file tối đa từ application.properties
-    private long maxFileSize;
-
     @Override
     public List<String> uploadFile(List<MultipartFile> multipartFiles) throws IOException {
         List<String> successMessages = new ArrayList<>();
         for (MultipartFile file : multipartFiles) {
-            if (file.getSize() > maxFileSize) {
-                // File size exceeds limit, handle accordingly
-                successMessages.add("File size exceeds the limit: " +
-                        file.getOriginalFilename());
-            } else {
-                successMessages.add(cloudinary.uploader()
-                        .upload(file.getBytes(),
-                                Map.of("public_id", UUID.randomUUID().toString()))
-                        .get("url")
-                        .toString());
-            }
+            successMessages.add(cloudinary.uploader()
+                    .upload(file.getBytes(),
+                            Map.of("public_id", UUID.randomUUID().toString()))
+                    .get("url")
+                    .toString());
         }
         return successMessages;
     }
